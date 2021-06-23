e395be057a28b94b7f51b954c5478f790e7ad64f
wilder_code <74383722+wilder-code@users.noreply.github.com>
2021-06-23 16:37:40 +1000

    Add files via upload

--- cc/views.py
+++ cc/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from cc.form import SignupForm
-
+from cc.models import User,User_Type_1,User_Type_2
 # Create your views here.
 
 def signup(request):
@@ -9,8 +9,20 @@ def signup(request):
         signup_form = SignupForm
     else:
         signup_form = SignupForm(request.POST)
-        
 
+    username = '2'
+    email = '1@q.com'
+    password = '1'
+
+    user = User.objects.create_user(
+        username=username,
+        password=password,
+        email=email
+    )
+    usertype_1 = User_Type_1.objects.create(user=user, user_type_1_teyoushuxing='frank')
+
+    #except Exception as e:
+        #return print('注册失败')
 
     return render(request = request,
                   template_name = "cc/test_sign.html",
